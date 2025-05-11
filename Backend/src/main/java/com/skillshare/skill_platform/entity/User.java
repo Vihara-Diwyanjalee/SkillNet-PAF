4872e61ee53a04002b62939ec3b15968f1330ccb
Deesara07 <it22907066@my.sliit.lk>
2025-05-11 08:06:44 +0530

    Update Entity User

--- Backend/src/main/java/com/skillshare/skill_platform/entity/User.java
+++ Backend/src/main/java/com/skillshare/skill_platform/entity/User.java
@@ -3,6 +3,7 @@ package com.skillshare.skill_platform.entity;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 
 @Document(collection = "users")
 @Data
@@ -11,6 +12,11 @@ public class User {
     private String id;
     private String email;
     private String name;
+    private String password;
     private String oauthProvider;
     private String oauthId;
+    
+    @DBRef
+    private UserProfile userProfile;
+
 }
