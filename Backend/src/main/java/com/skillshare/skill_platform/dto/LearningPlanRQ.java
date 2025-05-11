8e73e59308784e4acf8fed795fcb8df45b2d2f57
DasunFernando25 <146518662+DasunFernando25@users.noreply.github.com>
2025-05-11 09:58:46 +0530

    Update LearningPlanRQ.java

--- Backend/src/main/java/com/skillshare/skill_platform/dto/LearningPlanRQ.java
+++ Backend/src/main/java/com/skillshare/skill_platform/dto/LearningPlanRQ.java
@@ -1,16 +1,24 @@
 package com.skillshare.skill_platform.dto;
 
+import com.skillshare.skill_platform.entity.Resource;
+import com.skillshare.skill_platform.entity.Topic;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Data;
 
 @Data
 public class LearningPlanRQ {
 
-  private String learningPlanName;
-  private String learningPlanDescription;
-  private String stream;
+  private String id;
+  private String title;
+  private String description;
+  private String subject;
   private LocalDateTime createdAt;
-  private List<TopicRq> topics;
+  private List<Topic> topics = new ArrayList<>();
+  private List<Resource> resources = new ArrayList<>();
+  private Integer estimatedDays;
+  private Integer followers;
   private String userId;
+  private Boolean following;
 }
