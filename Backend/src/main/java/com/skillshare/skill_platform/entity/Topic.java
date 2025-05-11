e1386856df21d5f3b8e8ad6101e06b6505d9bd23
DasunFernando25 <146518662+DasunFernando25@users.noreply.github.com>
2025-05-11 09:59:55 +0530

    Update Topic.java

--- Backend/src/main/java/com/skillshare/skill_platform/entity/Topic.java
+++ Backend/src/main/java/com/skillshare/skill_platform/entity/Topic.java
@@ -5,6 +5,15 @@ import lombok.Data;
 @Data
 public class Topic {
 
-  private String name;
+  private String id;
+  private String title;
   private TopicStatus status;
+  
+  public boolean isCompleted() {
+    return status == TopicStatus.COMPLETED;
+  }
+  
+  public void setCompleted(boolean completed) {
+    this.status = completed ? TopicStatus.COMPLETED : TopicStatus.IN_PROGRESS;
+  }
 }
