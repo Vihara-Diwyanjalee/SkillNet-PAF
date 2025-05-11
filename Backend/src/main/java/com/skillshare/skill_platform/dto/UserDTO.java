0e5057d6819199a2df9ef5632dbe1a34ec661885
Deesara07 <it22907066@my.sliit.lk>
2025-05-11 08:04:54 +0530

    userDTO update

--- Backend/src/main/java/com/skillshare/skill_platform/dto/UserDTO.java
+++ Backend/src/main/java/com/skillshare/skill_platform/dto/UserDTO.java
@@ -1,10 +1,23 @@
 package com.skillshare.skill_platform.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
     private String id;
-    private String email;
     private String name;
+    private String username;
+    private String profilePicture;
+    private String email;
+    
+    // Add backwards compatibility for old code
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
