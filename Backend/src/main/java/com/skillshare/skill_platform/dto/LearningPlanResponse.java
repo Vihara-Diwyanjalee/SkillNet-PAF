7e262a621353e0561afa127518bfdab080d29077
DasunFernando25 <146518662+DasunFernando25@users.noreply.github.com>
2025-05-11 09:58:18 +0530

    Update LearningPlanResponse.java

--- Backend/src/main/java/com/skillshare/skill_platform/dto/LearningPlanResponse.java
+++ Backend/src/main/java/com/skillshare/skill_platform/dto/LearningPlanResponse.java
@@ -1,19 +1,31 @@
 package com.skillshare.skill_platform.dto;
 
+import com.skillshare.skill_platform.entity.Resource;
+import com.skillshare.skill_platform.entity.Topic;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LearningPlanResponse {
 
   private String id;
-  private String learningPlanName;
-  private String learningPlanDescription;
-  private String stream;
+  private String title;
+  private String description;
+  private String subject;
   private LocalDateTime createdAt;
-  private List<TopicRq> topics;
+  private List<Topic> topics;
+  private List<Resource> resources;
+  private Integer estimatedDays;
+  private Integer followers;
   private String userId;
+  private Boolean following;
+  private Integer completionPercentage;
+  private UserDTO user;
 }
